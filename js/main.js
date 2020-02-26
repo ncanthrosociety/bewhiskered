0f51a16cecf11d1447638057ec3ca7bf3e0d6af7
faelan <faelan.galanti@gmail.com>
2020-02-26 18:21:25 -0500

    Remove focus on click for scroll triggers.

--- js/main.js
+++ js/main.js
@@ -64,6 +64,7 @@
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
+    $(this).blur()
     $('.navbar-collapse').collapse('hide')
   })
 })(jQuery)
